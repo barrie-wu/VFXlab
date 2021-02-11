e72010b28385b9f516c48a94cb6dfc2c5b9ca82b
Barrie Wu <wu.barrie@gmail.com>
2021-02-11 10:30:03 -0500

    added aboutus with animations

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-const vfx = document.querySelector('.vfxSpan');
+const sfx = document.querySelector('.sfxSpan');
 const labs = document.querySelector('.labSpan');
 
 setTimeout(function() {
-    vfx.classList.add('logoFade')
-}, 2000)
+    sfx.classList.add('logoFade')
+}, 3000)
 
 setTimeout(function () {
     labs.classList.add('logoFade')
-}, 5000)
+}, 4500)
+
